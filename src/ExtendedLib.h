a987d99aa06ca12357bf3ab96ebd15e7265f1c6b
BlueStar5468 <bsuni5468@qq.com>
2026-06-17 23:03:35 +0800

    添加包含文件头

---
+++ src/ExtendedLib.h
@@ -0,0 +1,23 @@
+/*
+BS-Stm32 Extended lib
+Copyright (C) 2026 BlueStar5468
+SPDX-License-Identifier: GPL-3.0-only
+*/
+
+#ifndef __EXTENDEDLIB_H__
+#define __EXTENDEDLIB_H__
+
+//扩展库包含头文件
+
+#include "GPIO.h"
+#include "EXTI.h"
+#include "NVIC.h"
+#include "Tim.h"
+
+
+
+
+
+
+
+#endif // __EXTENDEDLIB_H__
